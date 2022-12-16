c56dcb0461fe96d1fdd25aaabd4d8f11db034476
Alexander Gozali <alexandergozali0@gmail.com>
2022-12-16 19:48:15 +0700

    Update: repair some file (nutrientdetail + sideeffects)

--- server/models/sideeffects.js
+++ server/models/sideeffects.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            Benefits.belongsTo(models.Product, {
+            SideEffects.belongsTo(models.Product, {
                 foreignKey: "productID",
             });
         }
