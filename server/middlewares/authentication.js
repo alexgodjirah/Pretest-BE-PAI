fb6716dc462989a5e8066b1a9603e8b9aec09562
Alexander Gozali <alexandergozali0@gmail.com>
2022-12-17 22:05:03 +0700

    Feat: Authentication using jsonwebtoken

---
+++ server/middlewares/authentication.js
@@ -0,0 +1,35 @@
+const { User } = require("../models");
+const { verifyToken } = require("../helpers/tokenHandler");
+
+const authentication = async (req, res, next) => {
+    // console.log(req);
+    const { access_token } = req.cookies;
+    // console.log(req.cookies);
+    console.log(access_token);
+    try {
+        if (access_token) {
+            const decodedData = await verifyToken(access_token);
+
+            const findUser = await User.findOne({
+                where: { username: decodedData.username },
+            });
+            if (!findUser) {
+                return res
+                    .status(404)
+                    .json({ message: "Sorry, the user is not found" });
+            }
+
+            req.user = decodedData;
+            next();
+        } else {
+            return res
+                .status(404)
+                .json({ message: "Access token is not found, Please login!!" });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = authentication;
--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -1,11 +1,15 @@
 const v1 = require("express").Router();
-
-// User Route
+const ProductController = require("../../controllers/productController");
+const authentication = require("../../middlewares/authentication");
 const userRoute = require("./user");
-v1.use("/user", userRoute);
 
 v1.get("/", (req, res) => {
     res.send("this is from v1");
 });
+
+// User Route
+v1.use("/user", userRoute);
+v1.use(authentication);
+v1.get("/product", ProductController.getAllProduct);
 
 module.exports = v1;
