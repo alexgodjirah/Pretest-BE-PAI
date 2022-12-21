9b9cbbf536d9b99e9296e522808b1a3097781891
Alexander Gozali <alexandergozali0@gmail.com>
2022-12-21 10:54:13 +0700

    Update: Some styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
+import FirstPage from "./components/FirstPage";
 import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <div className="App">
-            <nav>
+            <nav className="navbar--sticky">
                 <Navbar />
             </nav>
+            <main>
+                <FirstPage />
+            </main>
         </div>
     );
 }
