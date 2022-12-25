6b784aa49d1dcb8062f86d93cd71b57ed0878410
Alexander Gozali <alexandergozali0@gmail.com>
2022-12-25 15:28:47 +0700

    Feat: Navbar css (P.S still unfinished)

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,41 +2,89 @@ import React from "react";
 import "../styles/Navbar/index.css";
 
 export default function Navbar() {
+    React.useEffect(() => {
+        const body = document.body;
+        let lastScroll = 0;
+
+        window.addEventListener("scroll", () => {
+            const currentScroll = window.scrollY;
+
+            if (currentScroll <= 0) {
+                body.classList.remove("scroll-up");
+                return;
+            }
+
+            if (
+                currentScroll > lastScroll &&
+                !body.classList.contains("scroll-down")
+            ) {
+                body.classList.remove("scroll-up");
+                body.classList.add("scroll-down");
+            }
+
+            if (
+                currentScroll < lastScroll &&
+                body.classList.contains("scroll-down")
+            ) {
+                body.classList.remove("scroll-down");
+                body.classList.add("scroll-up");
+            }
+
+            lastScroll = currentScroll;
+        });
+    });
+
     return (
-        <>
-            <nav className="navbar container">
-                <div>
-                    <h3 className="font--h3">Logo</h3>
-                </div>
+        <header className="header container">
+            <div className="navbar--logo">
+                <h3 className="font--h3">Logo</h3>
+            </div>
+            <nav className="navbar">
                 <div className="navbar--items">
                     <ul className="items--list">
                         <li className="list--items">
-                            <h3 className="font--h3">About Us</h3>
+                            <a href="/about" className="font--h3">
+                                About Us
+                            </a>
                         </li>
                         <li className="list--items">
-                            <h3 className="font--h3">Impact</h3>
+                            <a href="#" className="font--h3">
+                                Impact
+                            </a>
                         </li>
                         <li className="list--items">
-                            <h3 className="font--h3">Products</h3>
+                            <a href="#" className="font--h3">
+                                Products
+                            </a>
                         </li>
                         <li className="list--items">
-                            <h3 className="font--h3">Sectors</h3>
+                            <a href="#" className="font--h3">
+                                Sectors
+                            </a>
                         </li>
                         <li className="list--items">
-                            <h3 className="font--h3">Blog</h3>
+                            <a href="#" className="font--h3">
+                                Blog
+                            </a>
                         </li>
                         <li className="list--items">
-                            <h3 className="font--h3">Career</h3>
+                            <a href="#" className="font--h3">
+                                Career
+                            </a>
                         </li>
                         <li className="list--items">
-                            <h3 className="font--h3">Contact</h3>
+                            <a href="#" className="font--h3">
+                                Contact
+                            </a>
                         </li>
                         <li className="list--items">
-                            <h3 className="font--h3">Language</h3>
+                            <a href="#" className="font--h3">
+                                Language
+                            </a>
                         </li>
                     </ul>
                 </div>
             </nav>
-        </>
+        </header>
     );
 }
